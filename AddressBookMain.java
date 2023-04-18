88f8ca8d85b6e88eacfdbb198926c28202a22732
“ShubhamvShrikhande” <shubhamvshri99@gmail.com>
2023-04-18 13:47:13 +0530

    create contact

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -3,7 +3,8 @@ package com.addressBook;
 public class AddressBookMain {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book program");
-		
+		AddressBook addressBook = new AddressBook();
+		addressBook.createContact();
 	}
 
 }
